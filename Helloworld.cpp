be7c2f22f3848af8699cb64ce7016f6a71fd9e1a
luckydog228 <128289395+luckydog228@users.noreply.github.com>
2023-05-30 10:18:41 -0400

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -3,6 +3,6 @@ include <string>
 int main() {
 std::string name;
 std::cout << "Please enter name";
-std::cin >> name; //enter name
+std::cin >> name; //введите имя
 std::cout << "Hello world from " << name;
 }
