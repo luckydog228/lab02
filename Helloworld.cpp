793e97f64bfcbf01376e88ab32c7d72fc24eb32c
luckydog228 <bocheellena@yandex.ru>
2023-05-30 10:07:58 -0400

    changed codestyle

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,8 +1,8 @@
-include <iostream>
-include <string>
-int main() {
-std::string name;
-std::cout << "Please enter name";
-std::cin >> name; //введите имя
-std::cout << "Hello world from " << name;
+include<iostream> include<string> int
+main()
+{
+  std::string name;
+  std::cout << "Please enter name";
+  std::cin >> name; //вводим имя
+  std::cout << "Hello world from " << name;
 }
